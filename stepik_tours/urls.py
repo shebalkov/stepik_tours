a9af5588e30c1227befe347350f7465769796218
Mike <renegadems@gmail.com>
2020-05-01 18:53:40 +0300

    args fix

--- stepik_tours/urls.py
+++ stepik_tours/urls.py
@@ -4,5 +4,5 @@ from tours.views import MainView, DepartureView, TourView
 urlpatterns = [
     path('', MainView.as_view()),
     path('departure/<str:departure>', DepartureView.as_view()),
-    path('/tour/<int:id>/', TourView.as_view()),
+    path('tour/<int:id>/', TourView.as_view()),
 ]
--- tours/views.py
+++ tours/views.py
@@ -3,15 +3,15 @@ from django.views import View
 
 
 class MainView(View):
-    def get(self, request):
+    def get(self, request, *args, **kwargs):
         return render(request, 'index.html')
 
 
 class DepartureView(View):
-    def get(self, request):
+    def get(self, request, *args, **kwargs):
         return render(request, 'departure.html')
 
 
 class TourView(View):
-    def get(self, request):
+    def get(self, request, *args, **kwargs):
         return render(request, 'tour.html')
